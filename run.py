b59123a6e66e5dc9023030b6019c92788f95668e
mayankhurana <146905878+mayankhurana@users.noreply.github.com>
2024-04-08 12:49:32 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from app import app
-app=app
+from app import app  # Importing the Flask app instance from the app module
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # Run the Flask app with debugging enabled if executed directly
